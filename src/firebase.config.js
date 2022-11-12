afe446fb4bae2a96d6710739878917861a7e097b
Nahid005 <nahidhassan9258@gmail.com>
2022-11-13 01:13:03 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+import {initializeApp, getApp, getApps} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9w4xp8I8JwpRCg4N9lSsrOb1_GeNA5K8",
+    authDomain: "food-delivery-c7235.firebaseapp.com",
+    projectId: "food-delivery-c7235",
+    storageBucket: "food-delivery-c7235.appspot.com",
+    messagingSenderId: "167031842930",
+    appId: "1:167031842930:web:b6be9fa5426b6cb6947aeb"
+  };
